5190a04c80fb058f343508de0d16981108d46fe0
Steelwix <mhunmael@hotmail.com>
2023-02-15 17:24:39 +0100

    crit system on player

--- assets/react/controllers/Fight.jsx
+++ assets/react/controllers/Fight.jsx
@@ -1,3 +1,4 @@
+
 import React, { useState } from 'react';
 import { useForm } from "react-hook-form";
 
@@ -7,20 +8,33 @@ function Fight(props) {
 
     const [player, setPlayer] = useState(playerData);
     const [boss, setBoss] = useState(bossData);
-
-    const handlerPlayerAttack = () => {
-        setBoss({ ...boss, hp: boss.hp - playerData.physicalDamage });
-        console.log("The boss took ", playerData.physicalDamage);
-
-    }
     while (player.hp > 0 && boss.hp > 0) {
-        return (<section class="container"><div class="row"><div class="col-12 text-center"><h1>Vous rencontrez {bossData.name}</h1></div>
-            <div class="col-6"><p>{playerData.name} : {playerData.hp}</p>
+        console.log(player);
+        const luckCall = () => {
+            const randomValue = Math.random();
+            return randomValue;
+        }
+        const handlerPlayerAttack = () => {
+            let damage = player.physicalDamage;
+            let luck = luckCall();
+            if (player.critChance > luck) {
+                damage = player.physicalDamage * player.critDamage;
+                console.log("CRITIQUE ", damage, luck);
+            }
+            setBoss({ ...boss, hp: boss.hp - damage });
+            console.log("Le boss a subi ", damage);
+            setPlayer({ ...player, hp: player.hp - boss.physicalDamage });
+            console.log("Vous subissez ", boss.physicalDamage);
+
+        }
+
+        return (<section class="container"><div class="row"><div class="col-12 text-center"><h1>Vous rencontrez {boss.name}</h1></div>
+            <div class="col-6"><p>{player.name} : {player.hp}</p>
                 <button onClick={handlerPlayerAttack}>Attaquer</button><br />
                 <button>Utiliser une relique</button><br />
                 <button>Utiliser un objet</button><br />
                 <button>Fuir</button><br />
-            </div><div class="col-6"><p>{bossData.name} : {boss.hp}</p></div></div></section >);
+            </div><div class="col-6"><p>{boss.name} : {boss.hp}</p></div></div></section >);
     }
 
     return (<div>NTM</div>);
